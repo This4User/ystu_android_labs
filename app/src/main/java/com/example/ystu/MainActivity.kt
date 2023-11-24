5f4a3a18d1e9da6db3248673400b1c9dede676b3
This4User <danilagamelol@gmail.com>
2023-11-24 11:39:19 +0300

    feat(second-lab): move ui to fragment

--- app/src/main/java/com/example/ystu/MainActivity.kt
+++ app/src/main/java/com/example/ystu/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.ystu
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ystu.databinding.ActivityMainBinding
 import com.example.ystu.viewmodel.CounterViewModel
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,25 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val counterViewModel = ViewModelProvider(this)[CounterViewModel::class.java]
-        counterViewModel.init()
 
-        val counterObserver = Observer<Int> { newCounter ->
-            binding.numberField.text = newCounter.toString()
-        }
-
-        counterViewModel.counter.observe(this, counterObserver)
-
-        binding.numberField.text = counterViewModel.counter.value.toString()
-
-        binding.plusButton.setOnClickListener {
-            counterViewModel.increment()
-        }
-        binding.minusButton.setOnClickListener {
-            counterViewModel.decrement()
-        }
         binding.secondScreen.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
+            val counterViewModel = ViewModelProvider(this)[CounterViewModel::class.java]
+            counterViewModel.init()
             intent.putExtra("counter", counterViewModel.counter.value.toString())
             startActivity(intent)
         }
--- app/src/main/java/com/example/ystu/fragment/CounterFragment.kt
+++ app/src/main/java/com/example/ystu/fragment/CounterFragment.kt
@@ -1,4 +1,53 @@
 package com.example.ystu.fragment
 
-class CounterFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.ystu.R
+import com.example.ystu.viewmodel.CounterViewModel
+
+class CounterFragment : Fragment(R.layout.fragment_counter) {
+    private var inflatedView: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        inflatedView = inflater.inflate(R.layout.fragment_counter, container, false)
+        val textView = inflatedView?.findViewById<TextView>(R.id.number_field)
+        val minusButton = inflatedView?.findViewById<TextView>(R.id.minus_button)
+        val plusButton = inflatedView?.findViewById<TextView>(R.id.plus_button)
+
+        val counterViewModel = ViewModelProvider(this)[CounterViewModel::class.java]
+        counterViewModel.init()
+
+        val counterObserver = Observer<Int> { newCounter ->
+            textView?.text = newCounter.toString()
+        }
+
+        counterViewModel.counter.observe(viewLifecycleOwner, counterObserver)
+
+        textView?.text = counterViewModel.counter.value.toString()
+
+        plusButton?.setOnClickListener {
+            counterViewModel.increment()
+        }
+        minusButton?.setOnClickListener {
+            counterViewModel.decrement()
+        }
+
+        return inflatedView
+    }
+
+    companion object {
+    }
 }
