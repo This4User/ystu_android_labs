bf1908b72756c6e4bb106ca4bc4ca4bedcab0abd
This4User <danilagamelol@gmail.com>
2023-11-27 10:37:05 +0300

    feat(second-lab): change app id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.ystu"
+        applicationId = "com.example.ystu2"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
